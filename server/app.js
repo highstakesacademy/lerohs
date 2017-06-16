88f73dd6829cebb066e843fcc00d709e6f011430
Michael Klemersson <mklemersson.dev@gmail.com>
2017-06-16 01:18:45 -0300

    add assets folder to serve static files

--- server/app.js
+++ server/app.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-
 const app = express();
+
+app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendfile('front/index.html')
 });
 
-app.get('/admin', (req, res) => {
-  res.sendfile('front/admin.html');
+app.get('/admin/dashboard', (req, res) => {
+  res.sendfile('front/admin/dashboard.html');
 });
-
-
 
 export { app };
